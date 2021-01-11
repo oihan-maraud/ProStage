0f387e2fe012bdc1499d02a7a5ed8e6a49997ad5
oihan Maraud <omaraud@iutbayonne.univ-pau.fr>
2021-01-11 19:39:51 +0100

    Premiere utilisation de la librairie Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,8 +10,11 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+      //Creation de generateurs de donnees Faker
+      $faker = \Faker\Factory::create('fr_FR');
+
        $formation1 = new Formation();
-       $formation1->setIntitule("Dut info");
+       $formation1->setIntitule($faker->realText($maxNbChars = 50, $indexSize = 2));
        $formation1->setNiveau("2e annee");
        $formation1->setVille("Bayonne");
 
