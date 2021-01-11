9e88c78fde51cf4cf8d8306454208fcc793cd280
oihan Maraud <omaraud@iutbayonne.univ-pau.fr>
2021-01-11 21:49:34 +0100

    Génération de formation et envoie en BD

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,13 +13,24 @@ class AppFixtures extends Fixture
       //Creation de generateurs de donnees Faker
       $faker = \Faker\Factory::create('fr_FR');
 
-       $formation1 = new Formation();
-       $formation1->setIntitule($faker->realText($maxNbChars = 50, $indexSize = 2));
-       $formation1->setNiveau("2e annee");
-       $formation1->setVille("Bayonne");
+      $nbFormation = 5;
 
-       $manager->persist($formation1);
+      for($i = 1; $i <= $nbFormation; $i++ ){
+          // création d'une nouvelle formation
+           $formation1 = new Formation();
+           //Génération d'un non de formations
+           $nomFormation = $faker->regexify("Formation [1-9]");
+           //Définition du nom de la formation
+           $formation1->setIntitule($nomFormation);
+           //Définition du niveau de la formation
+           $formation1->setNiveau($faker->regexify("[1-2]e année"));
+           //Définition de la ville de la formation
+           $formation1->setVille($faker->city());
+           //Enregistrement de la formation créé
+           $manager->persist($formation1);
+       }
 
+       // Envoyer les données en BD
         $manager->flush();
     }
 }
